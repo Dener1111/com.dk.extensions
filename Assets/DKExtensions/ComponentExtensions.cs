508398f868c2ebfbc42a99b05bda4f73f2fc7aee
DK <mr.dener1111@gmail.com>
2022-07-08 13:35:27 +0300

    Initial commit

---
+++ Assets/DKExtensions/ComponentExtensions.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public static class ComponentExtensions
+{
+    /// <summary>
+    /// Attaches a component to the given component's game object.
+    /// </summary>
+    /// <param name="component">Component.</param>
+    /// <returns>Newly attached component.</returns>
+    public static T AddComponent<T>(this Component component) where T : Component
+    {
+        return component.gameObject.AddComponent<T>();
+    }
+
+    /// <summary>
+    /// Gets a component attached to the given component's game object.
+    /// If one isn't found, a new one is attached and returned.
+    /// </summary>
+    /// <param name="component">Component.</param>
+    /// <returns>Previously or newly attached component.</returns>
+    public static T GetOrAddComponent<T>(this Component component) where T : Component
+    {
+        return component.GetComponent<T>() ?? component.AddComponent<T>();
+    }
+
+    /// <summary>
+    /// Checks whether a component's game object has a component of type T attached.
+    /// </summary>
+    /// <param name="component">Component.</param>
+    /// <returns>True when component is attached.</returns>
+    public static bool HasComponent<T>(this Component component) where T : Component
+    {
+        return component.GetComponent<T>() != null;
+    }
+
+    /// <summary>Inactivates gameobject</summary>
+	public static void SetInactive(this Component component)
+    {
+        component.gameObject.SetActive(false);
+    }
+
+    /// <summary>Activates gameobject</summary>
+    public static void SetActive(this Component component)
+    {
+        component.gameObject.SetActive(true);
+    }
+
+    /// <summary>Activates gameobject</summary>
+	public static void SetActive(this Component component, bool value)
+    {
+        component.gameObject.SetActive(value);
+    }
+
+    /// <summary>Inactivates gameObject</summary>
+    /// <param name="delay">Time after which gameObject will be Inactivated.</param>
+	public static async void SetInactive(this Component component, float delay)
+    {
+        await new WaitForSeconds(delay);
+        component.gameObject.SetActive(false);
+    }
+
+    /// <summary>Activates gameObject</summary>
+    /// <param name="delay">Time after which gameObject will be Activated.</param>
+    public static async void SetActive(this Component component, float delay)
+    {
+        await new WaitForSeconds(delay);
+        component.gameObject.SetActive(true);
+    }
+
+    /// <summary>Inactivates gameObject</summary>
+    /// <param name="frames">Frames after which gameObject will be Deactivated.</param>
+	public static async void SetInactive(this Component component, int frames)
+    {
+        for (int i = 0; i < frames; i++)
+            await new WaitForEndOfFrame();
+        component.gameObject.SetActive(false);
+    }
+
+    /// <summary>Activates gameObject</summary>
+    /// <param name="frames">Frames after which gameObject will be Activated.</param>
+    public static async void SetActive(this Component component, int frames)
+    {
+        for (int i = 0; i < frames; i++)
+            await new WaitForEndOfFrame();
+        component.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/DKExtensions/ListExtensions.cs
@@ -0,0 +1,122 @@
+using System.Linq;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+
+public static partial class Extensions
+{
+    /// <summary>Returns random element of the List</summary>
+    public static T GetRandom<T>(this List<T> list)
+    {
+        if (list.Count == 0)
+            return default(T);
+
+        return list[Random.Range(0, list.Count)];
+    }
+
+    /// <summary>Returns random element of the List</summary>
+    public static T GetRandom<T>(this List<T> list, out int index)
+    {
+        index = -1;
+        if (list.Count == 0)
+            return default(T);
+
+        index = Random.Range(0, list.Count);
+        return list[index];
+    }
+
+    /// <summary>Returns random element of the List</summary>
+    public static T GetRandomWithout<T>(this List<T> list, int without)
+    {
+        if (list.Count == 0)
+            return default(T);
+
+        return list[GetRandomIndexWithout(list, without)];
+    }
+
+    /// <summary>Returns random element of the List</summary>
+    public static T GetRandomWithout<T>(this List<T> list, int without, out int index)
+    {
+        index = -1;
+        if (list.Count == 0)
+            return default(T);
+
+        index = GetRandomIndexWithout(list, without);
+        return list[index];
+    }
+
+    /// <summary>Returns random element of the List</summary>
+    /// <param name="index">Start index</param>
+    public static T GetRandom<T>(this List<T> list, int index)
+    {
+        if (list.Count == 0)
+            return default(T);
+
+        if (list.Count <= index)
+            return list[0];
+
+        return list[Random.Range(index, list.Count)];
+    }
+
+    /// <summary>Заполняет массив</summary>
+    /// <param name="value">Значение для заполнения массива</param>
+    /// <example>
+    /// <code>
+    /// var array = new int[10];
+    /// array.Populate(5);
+    /// </code>
+    /// </example>
+    public static void Populate<T>(this T[] arr, T value)
+    {
+        for (var i = 0; i < arr.Length; i++)
+            arr[i] = value;
+    }
+
+    /// <summary>Пепреводит лист с читаемый вид</summary>
+    public static string Print<T>(this IList<T> list)
+    {
+        var res = "[ " + string.Join(", ", list.Select(s => $"{s}\n")) + " ]";
+        return res;
+    }
+
+    // public static string Print(this IList list)
+    // {
+    // 	var res = "[ " + string.Join(", ", list.(s => $"{s}")) + " ]";
+    // 	return res;
+    // }
+
+    /// <summary>Shuffles all alemnts in the List</summary>
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        System.Random rng = new System.Random();
+
+        var n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            var k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    /// <summary>Returns random index of the List</summary>
+    public static int GetRandomIndex<T>(this List<T> list)
+    {
+        if (list.Count == 0)
+            return -1;
+
+        return Random.Range(0, list.Count);
+    }
+
+    /// <summary>Returns random index of the List with exclusion</summary>
+    /// <param name="exclusion">exclusion index</param>
+    public static int GetRandomIndexWithout<T>(this List<T> list, int exclusion = -1)
+    {
+        if (list.Count == 0)
+            return -1;
+
+        return IntExtensions.RandomRangeWithout(0, list.Count, exclusion);
+    }
+}
