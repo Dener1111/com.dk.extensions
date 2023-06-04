9cdd520c51d0342526611d17396251385991d395
Dener1111 <47250868+Dener1111@users.noreply.github.com>
2023-06-04 20:17:30 +0300

    Update UTask 2.cs

--- DKExtensions/UniTask/UTask.cs
+++ DKExtensions/UniTask/UTask.cs
@@ -1,25 +1,18 @@
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
-using Cysharp.Threading.Tasks.Internal;
-using System;
-using System.Collections;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using UnityEngine;
 using Cysharp.Threading.Tasks;
 
 public partial struct UniTask
 {
-    public static UniTask WaitForSeconds(float secondsDelay, bool ignoreTimeScale = false, PlayerLoopTiming delayTiming = PlayerLoopTiming.Update, CancellationToken cancellationToken = default(CancellationToken))
+    public static UniTask WaitForSeconds(float secondsDelay, bool ignoreTimeScale = false, PlayerLoopTiming delayTiming = PlayerLoopTiming.Update, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
     {
-        var delayTimeSpan = TimeSpan.FromSeconds(secondsDelay);
-        return Delay(delayTimeSpan, ignoreTimeScale, delayTiming, cancellationToken);
+        var delayTimeSpan = System.TimeSpan.FromSeconds(secondsDelay);
+        return Cysharp.Threading.Tasks.Internal.Delay(delayTimeSpan, ignoreTimeScale, delayTiming, cancellationToken);
     }
 
-    public static UniTask WaitForSeconds(float secondsDelay, DelayType delayType, PlayerLoopTiming delayTiming = PlayerLoopTiming.Update, CancellationToken cancellationToken = default(CancellationToken))
+    public static UniTask WaitForSeconds(float secondsDelay, DelayType delayType, PlayerLoopTiming delayTiming = PlayerLoopTiming.Update, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
     {
-        var delayTimeSpan = TimeSpan.FromSeconds(secondsDelay);
-        return Delay(delayTimeSpan, delayType, delayTiming, cancellationToken);
+        var delayTimeSpan = System.TimeSpan.FromSeconds(secondsDelay);
+        return Cysharp.Threading.Tasks.Internal.Delay(delayTimeSpan, delayType, delayTiming, cancellationToken);
     }
 
 }
