418a8e845afea99f890d387cb11449ffaf21af08
Dener1111 <47250868+Dener1111@users.noreply.github.com>
2023-12-17 00:22:04 +0300

    can name animator override controller

--- DKExtensions/RuntimeAnimatorControllerExtensions.cs
+++ DKExtensions/RuntimeAnimatorControllerExtensions.cs
@@ -5,10 +5,11 @@ public static partial class RuntimeAnimatorControllerExtension
     ///<summary>
     ///Returns AnimatorOverrideController
     ///</summary>
-    public static AnimatorOverrideController CreateOverride(this RuntimeAnimatorController animatorController)
+    public static AnimatorOverrideController CreateOverride(this RuntimeAnimatorController animatorController, string overrideName = "")
     {
         AnimatorOverrideController animatorOverrideController= new AnimatorOverrideController();
         animatorOverrideController.runtimeAnimatorController= animatorController;
+        animatorOverrideController.name = overrideName;
         return animatorOverrideController;
     }
 }
