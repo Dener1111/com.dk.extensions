86e6b38d06198f7df697c9630d02015c5cf5ef7c
DK <47250868+Dener1111@users.noreply.github.com>
2025-08-27 17:40:25 +0200

    SkinnedMeshRendererExtensions cancelation token

--- DKExtensions/SkinnedMeshRendererExtensions.cs
+++ DKExtensions/SkinnedMeshRendererExtensions.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -7,26 +8,43 @@ public static partial class SkinnedMeshRendererExtensions
     /// <summary>
     /// Animates blend shape to endValue 
     /// </summary>
-    public static async Task AnimateBlendShape(this SkinnedMeshRenderer rend, int id, float beginValue = 0, float endValue = 100, float time = 1f)
+    public static async UniTask AnimateBlendShape(this SkinnedMeshRenderer rend, int id, float beginValue = 0f, float endValue = 100f, float time = 1f)
+    {
+        await rend.AnimateBlendShape(id, rend.GetCancellationTokenOnDestroy(), beginValue, endValue, time);
+    }
+
+    /// <summary>
+    /// Animates blend shape to endValue 
+    /// </summary>
+    public static async UniTask AnimateBlendShape(this SkinnedMeshRenderer rend, int id, CancellationToken token, float beginValue = 0f, float endValue = 100f, float time = 1f)
     {
         float elapsed = 0;
         while (elapsed < time)
         {
-            await UniTask.NextFrame();
+            await UniTask.WaitForEndOfFrame(token);
 
             elapsed += Time.deltaTime;
             rend.SetBlendShapeWeight(id, Mathf.Lerp(beginValue, endValue, Mathf.InverseLerp(0, time, elapsed)));
         }
+
         rend.SetBlendShapeWeight(id, endValue);
+    }
+    
+    /// <summary>
+    /// Animates blend shape to endValue and then back
+    /// </summary>
+    public static async UniTask AnimateBlendShapePunch(this SkinnedMeshRenderer rend, int id, float beginValue = 0f, float endValue = 100f, float time = 1f)
+    {
+        await rend.AnimateBlendShapePunch(id, rend.GetCancellationTokenOnDestroy(), beginValue, endValue, time);
     }
 
     /// <summary>
     /// Animates blend shape to endValue and then back
     /// </summary>
-    public static async Task AnimateBlendShapePunch(this SkinnedMeshRenderer rend, int id, float beginValue = 0, float endValue = 100, float time = 1f)
+    public static async UniTask AnimateBlendShapePunch(this SkinnedMeshRenderer rend, int id, CancellationToken token, float beginValue = 0f, float endValue = 100f, float time = 1f)
     {
         time *= .5f;
-        await AnimateBlendShape(rend, id, beginValue, endValue, time);
-        await AnimateBlendShape(rend, id, endValue, beginValue, time);
+        await AnimateBlendShape(rend, id, token, beginValue, endValue, time);
+        await AnimateBlendShape(rend, id, token, endValue, beginValue, time);
     }
 }
