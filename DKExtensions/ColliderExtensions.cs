4eb3a0506604634d1561b91bf939ce3eb0fc3e6d
DK <47250868+Dener1111@users.noreply.github.com>
2025-08-27 17:20:14 +0200

    ColliderExtensions cancelation token

--- DKExtensions/ColliderExtensions.cs
+++ DKExtensions/ColliderExtensions.cs
@@ -8,15 +8,9 @@ public static partial class ColliderExtensions
     {
         collider.enabled = true;
     }
-
-    /// <summary>Disables Collider</summary>
-	public static void SetDisabled(this Collider collider)
-    {
-        collider.enabled = false;
-    }
-
+    
     /// <summary>Enables or disables Collider</summary>
-	public static void SetEnabled(this Collider collider, bool enabled)
+    public static void SetEnabled(this Collider collider, bool enabled)
     {
         collider.enabled = enabled;
     }
@@ -27,7 +21,36 @@ public static partial class ColliderExtensions
     {
         collider.SetEnabled(true, delay);
     }
+    
+    /// <summary>Enables or disables Collider</summary>
+    /// <param name="delay">Time after which Collider will be enabled or disabled.</param>
+    public static async void SetEnabled(this Collider collider, bool enabled, float delay)
+    {
+        await UniTask.WaitForSeconds(delay, cancellationToken: collider.GetCancellationTokenOnDestroy());
+        collider.enabled = enabled;
+    }
+    
+    /// <summary>Enables Collider</summary>
+    /// <param name="frames">Frames after which Collider will be enabled or disabled.</param>
+    public static void SetEnabled(this Collider collider, int frames)
+    {
+        collider.SetEnabled(true, frames);
+    }
+    
+    /// <summary>Enables or disables Collider</summary>
+    /// <param name="frames">Frames after which Collider will be enabled or disabled.</param>
+    public static async void SetEnabled(this Collider collider, bool enabled, int frames)
+    {
+        await UniTask.DelayFrame(frames, cancellationToken: collider.GetCancellationTokenOnDestroy());
+        collider.enabled = enabled;
+    }
 
+    /// <summary>Disables Collider</summary>
+	public static void SetDisabled(this Collider collider)
+    {
+        collider.enabled = false;
+    }
+    
     /// <summary>Disables Collider</summary>
     /// <param name="delay">Time after which Collider will be enabled or disabled.</param>
     public static void SetDisabled(this Collider collider, float delay)
@@ -35,33 +58,10 @@ public static partial class ColliderExtensions
         collider.SetEnabled(false, delay);
     }
 
-    /// <summary>Enables or disables Collider</summary>
-    /// <param name="delay">Time after which Collider will be enabled or disabled.</param>
-    public static async void SetEnabled(this Collider collider, bool enabled, float delay)
-    {
-	await UniTask.WaitForSeconds(delay);
-        collider.enabled = enabled;
-    }
-
-    /// <summary>Enables Collider</summary>
-    /// <param name="frames">Frames after which Collider will be enabled or disabled.</param>
-    public static void SetEnabled(this Collider collider, int frames)
-    {
-        collider.SetEnabled(true, frames);
-    }
-
     /// <summary>Disables Collider</summary>
     /// <param name="frames">Frames after which Collider will be enabled or disabled.</param>
     public static void SetDisabled(this Collider collider, int frames)
     {
         collider.SetEnabled(false, frames);
-    }
-
-    /// <summary>Enables or disables Collider</summary>
-    /// <param name="frames">Frames after which Collider will be enabled or disabled.</param>
-    public static async void SetEnabled(this Collider collider, bool enabled, int frames)
-    {
-        await UniTask.DelayFrame(frames);
-        collider.enabled = enabled;
     }
 }
